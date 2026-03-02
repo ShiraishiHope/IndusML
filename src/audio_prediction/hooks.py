3e4f68b9ee7da9d97c3e3991ff30a8f7608a4403
Shiraishi <ryan.mazri@yahoo.fr>
2026-03-02 18:34:31 +0100

    tried fixing, not working

---
+++ src/audio_prediction/hooks.py
@@ -0,0 +1,20 @@
+import mlflow
+from kedro.framework.hooks import hook_impl
+
+
+class MLflowHook:
+    @hook_impl
+    def before_pipeline_run(self, run_params, pipeline, catalog):
+        mlflow.set_tracking_uri("./mlruns")
+        mlflow.set_experiment("audio_prediction")
+        mlflow.start_run()
+
+    @hook_impl
+    def after_pipeline_run(self, run_params, pipeline, catalog):
+        if mlflow.active_run():
+            mlflow.end_run()
+
+    @hook_impl
+    def on_pipeline_error(self, error, run_params, pipeline, catalog):
+        if mlflow.active_run():
+            mlflow.end_run(status="FAILED")
