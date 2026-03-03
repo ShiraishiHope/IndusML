a7f9826ba97bdf8df1225d96f8feab875a3fbf4f
Shiraishi <ryan.mazri@yahoo.fr>
2026-03-03 12:23:24 +0100

    fix doublelogging

--- src/audio_prediction/hooks.py
+++ src/audio_prediction/hooks.py
@@ -5,12 +5,10 @@ from kedro.framework.hooks import hook_impl
 class MLflowHook:
     @hook_impl
     def before_pipeline_run(self, run_params, pipeline, catalog):
-        print(">>> MLflow hook triggered")
-        mlflow.set_tracking_uri("./mlruns")
+        mlflow.set_tracking_uri("sqlite:///mlflow.db")  
         mlflow.set_experiment("audio_prediction")
-        mlflow.autolog()
         mlflow.start_run()
-        print(f">>> Active run: {mlflow.active_run().info.run_id}")
+        mlflow.keras.autolog(disable=True)
 
     @hook_impl
     def after_pipeline_run(self, run_params, pipeline, catalog):
